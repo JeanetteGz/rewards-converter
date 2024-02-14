b66d70963f97883c904115cee617993398c7aa5c
Jeanettegz <Jeanetteglz99@gmail.com>
2024-02-14 17:57:23 -0500

    Completed task 1

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -1,30 +1,37 @@
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class RewardValueTests {
+    public class RewardValueTests {
 
-    @Test
-    void create_with_cash_value() {
-        double cashValue = 100;
-        var rewardValue = new RewardValue(cashValue);
-        assertEquals(cashValue, rewardValue.getCashValue());
-    }
+        @Test
+        void create_with_cash_value() {
+            double cashValue = 100;
+            var rewardValue = new RewardValue(cashValue);
+            assertEquals(cashValue, rewardValue.getCashValue());
+        }
 
-    @Test
-    void create_with_miles_value() {
-        int milesValue = 10000;
-        var rewardValue = new RewardValue(milesValue);
-        assertEquals(milesValue, rewardValue.getMilesValue());
-    }
+        @Test
+        void create_with_miles_value() {
+            int milesValue = 10000;
+            var rewardValue = new RewardValue(milesValue);
+            assertEquals(milesValue, rewardValue.getMilesValue());
+        }
 
-    @Test
-    void convert_from_cash_to_miles() {
-        assert false;
-    }
+        @Test
+        void convert_from_cash_to_miles() {
+            double cashValue = 100;
+            int CorrectMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+            var rewardValue = new RewardValue(cashValue);
+            assertEquals(CorrectMilesValue, rewardValue.getMilesValue());
+        }
 
-    @Test
-    void convert_from_miles_to_cash() {
-        assert false;
+        @Test
+        void convert_from_miles_to_cash() {
+            int milesValue = 10000;
+            double CorrectCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+            var rewardValue = new RewardValue(milesValue);
+            assertEquals(CorrectCashValue, rewardValue.getCashValue());
+        }
     }
-}
